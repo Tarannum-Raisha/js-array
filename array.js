c560f163df2e547f5b4b9e8692c5b19bd8dd393b
Tarannum-Raisha <raishatawsif@gmail.com>
2024-10-18 20:36:00 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,14 @@
+/**
+ *const num1 = 22;
+const num1 = 23;
+const num1 = 24;
+const num1 = 25;
+const num1 = 26;
+ * 
+ */
+
+const numbers = [22, 23, 24, 25, 26] ;
+const friends = ['abul', 'babul', 'kabul', 'cabul', 'bulbul'];
+const passed = [true, false, true, false];
+
+
---
+++ index.js
@@ -0,0 +1,16 @@
+const numbers = [11, 23, 234, 457, 45, 34];
+// console.log(numbers);
+
+// get element value by imdex
+console.log(numbers[3]);
+
+const fourth = numbers[3];
+console.log(fourth);
+
+// set or update element value by index
+ let abc = 23;
+ abc = 34;
+
+ numbers[2] = 999;
+ console.log(numbers);
+ 
